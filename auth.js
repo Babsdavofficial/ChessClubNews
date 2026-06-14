cdcc79c6f2ee006d94f4cc69a8a02b6ad9acbc7a
Babalola David Oluwaferanmi <121666073+Babsdavofficial@users.noreply.github.com>
2026-06-14 03:53:10 +0100

    Redirect to index.html after successful login

--- auth.js
+++ auth.js
@@ -88,7 +88,7 @@ if (loginBtn) {
       alert("🎉 Login successful!");
 
       // Go to the main page after login
-      window.location.href = "community.html";
+      window.location.href = "index.html";
 
     } catch (error) {
       alert("Login failed: " + error.message);
