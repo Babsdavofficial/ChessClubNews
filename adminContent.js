da11042d097116a38af82e862b01795401d46c01
Babalola David Oluwaferanmi <121666073+Babsdavofficial@users.noreply.github.com>
2026-06-17 04:25:47 +0100

    Update adminContent.js

--- adminContent.js
+++ adminContent.js
@@ -5,9 +5,9 @@ const publishBtn = document.getElementById("publishUpdateBtn");
 
 if (publishBtn) {
   publishBtn.addEventListener("click", () => {
-    const title = document.getElementById("updateTitle").value.trim();
-    const content = document.getElementById("updateContent").value.trim();
-    const image = document.getElementById("updateImage").files[0];
+   const title = document.getElementById("updateTitle").value.trim();
+   const content = document.getElementById("updateContent").value.trim();
+   const imageName = document.getElementById("updateImage").value.trim();
 
     if (!title || !content) {
       alert("Please enter both a title and an update message.");
