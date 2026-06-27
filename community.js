782c64b10d9684ce73eef97445cbeb9d3d5fbf2f
Babalola David Oluwaferanmi <121666073+Babsdavofficial@users.noreply.github.com>
2026-06-27 17:57:29 +0100

    Update community.js

--- community.js
+++ community.js
@@ -1,1 +1,15 @@
+import { db, auth } from "./firebase.js";
 
+import {
+  collection,
+  query,
+  where,
+  orderBy,
+  getDocs,
+  addDoc,
+  updateDoc,
+  doc,
+  getDoc,
+  onSnapshot,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
