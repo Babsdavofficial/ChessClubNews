87d318a08a6a77e3582281634f4b2fc47714f3e7
Babalola David Oluwaferanmi <121666073+Babsdavofficial@users.noreply.github.com>
2026-09-13 06:45:44 +0100

    Update cache version to v5.58

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // CHESS NEWS HUB — SERVICE WORKER
 // =====================================================
 
-const CACHE_NAME = "chess-news-hub-v5.57";
+const CACHE_NAME = "chess-news-hub-v5.58";
 
 const FILES_TO_CACHE = [
   "./",
