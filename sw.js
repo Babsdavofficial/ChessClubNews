8687c71f9c92f3cd8f486ff718b8096b18ed7d47
Babalola David Oluwaferanmi <121666073+Babsdavofficial@users.noreply.github.com>
2026-09-15 10:09:54 +0100

    Update service worker cache version to v5.94

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // CHESS NEWS HUB — SERVICE WORKER
 // =====================================================
 
-const CACHE_NAME = "chess-news-hub-v5.93";
+const CACHE_NAME = "chess-news-hub-v5.94";
 
 const FILES_TO_CACHE = [
   "./",
