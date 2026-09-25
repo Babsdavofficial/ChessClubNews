fc2ae6b5017fbb84e60486c4d8dbeab99ec1accd
Babalola David Oluwaferanmi <121666073+Babsdavofficial@users.noreply.github.com>
2026-09-25 12:51:43 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // CHESS NEWS HUB — SERVICE WORKER
 // =====================================================
 
-const CACHE_NAME = "chess-news-hub-v6.23";
+const CACHE_NAME = "chess-news-hub-v6.24";
 
 const FILES_TO_CACHE = [
   "./",
